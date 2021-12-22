27b78dd4b0560ee1d6402fc2f56a30bd7d086102
Aiden Fox-Ivey <71291879+Foxnaut@users.noreply.github.com>
2021-12-22 15:39:08 -0500

    Added boilerplate code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use web_view::*;
+
 fn main() {
-    println!("Hello, world!");
+    let html_content = "<html><body><h1>Hello World!</h1></body></html>";
+    
+    // long and ugly format, but builds well
+    web_view::builder
+        .title("Rose")
+        .content(Content::Html(html_content))
+        .size(320, 480)
+        .rezisable(true)
+        .debug(true)
+        .user_data(())
+        .invoke_handler(|_webview, _arg| Ok(()))
+        .run()
+        .unwrap();
 }
